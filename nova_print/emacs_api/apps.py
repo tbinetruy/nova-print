c1f139bca9e76f5bf5ff161e23f05f26d88e02c8
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-18 13:08:55 +0100

    created emacs_api app

---
+++ nova_print/emacs_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmacsApiConfig(AppConfig):
+    name = 'emacs_api'
---
+++ nova_print/emacs_api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import subprocess
+from django.template import loader
+
+def index(request):
+    cmd = "ls -la ."
+    sp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    stdout = sp.communicate()[0]
+    template = loader.get_template('emacs_api/index.html')
+
+
+    return HttpResponse(template.render({}, request))

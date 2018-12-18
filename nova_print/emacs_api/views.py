7a6d8abe23d5f29399c6af88381db58648f74fbe
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-18 17:05:19 +0100

    generate pdf on post request

--- nova_print/emacs_api/views.py
+++ nova_print/emacs_api/views.py
@@ -2,20 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
 from django.template import loader
+import json
 
+def create_pdf():
 
-def submit(request):
-    cmd = "ls -la ."
+    cmd = "cd /tmp && emacs output.org --batch --eval '(progn (load-file \"~/.emacs.d/nova-print/main.el\")(nova-print//export))' --kill"
     sp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     stdout = sp.communicate()[0]
+    return stdout
+
+def submit(request):
+    if request.method != 'POST':
+        return HttpResponse('ERROR')
+
+    org = json.loads(request.body)['org']
+    filename = "output.org"
+    with open(f"/tmp/{filename}", "w") as text_file:
+        text_file.write(org)
+
+    stdout = create_pdf()
 
     return HttpResponse(stdout)
 
-def index(request):
-    cmd = "ls -la ."
-    sp = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    stdout = sp.communicate()[0]
-    template = loader.get_template('emacs_api/index.html')
 
+def index(request):
+    template = loader.get_template('emacs_api/index.html')
 
     return HttpResponse(template.render({}, request))

95810e5dc393945c709d6b96ae19bf1051157a02
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-18 18:17:01 +0100

    added form for title, header color etc

--- nova_print/static/js/index.js
+++ nova_print/static/js/index.js
@@ -33,6 +33,57 @@ function iframe(props) {
 `;
 }
 
+function textbox(props) {
+  return `
+<label>${props.label}: </label><input id="${props.id}" />
+`;
+}
+
+function dropdown(props) {
+  return `
+<select>
+    ${props.options.map(e => `<option value="${e}">${e}</option>`).join("")}
+</select>
+`;
+}
+
+const colorList = [
+  "002684",
+  "0060c1",
+  "0069aa",
+  "0aaac7",
+  "119fd4",
+  "11d43f",
+  "123456",
+  "19cba9",
+  "3163c3",
+  "359db3",
+  "492b4c",
+  "4b5e85",
+  "4b7985",
+  "4e57a3",
+  "4f897d",
+  "53df5a",
+  "55a8e4",
+  "5b254e",
+  "5b4a84",
+  "5e6983",
+  "6b5c3c",
+  "707f9c",
+  "713975",
+  "7e5141",
+  "854c4b",
+  "874c6d",
+  "9e0000",
+  "a5435f",
+  "bd171f",
+  "ff1d52",
+  "ff2458",
+  "ff2d59",
+  "jb5c3c",
+  "pi7654",
+];
+
 function appDumb(props) {
   const b = button({title: "Submit", id: props.submitId});
   const t = textarea({
@@ -46,7 +97,13 @@ function appDumb(props) {
     children: [t, i],
     id: "main-panel",
   });
-  const children = [r, b];
+
+  const title = textbox({label: "Title", id: "title"});
+  const subtitle = textbox({label: "Subtitle", id: "subtitle"});
+  const colors = dropdown({options: colorList});
+  const doctype = dropdown({options: ["memo"]});
+  const headerInfo = column({children: [title, subtitle, colors, doctype]});
+  const children = [headerInfo, r, b];
 
   return `
 <div>

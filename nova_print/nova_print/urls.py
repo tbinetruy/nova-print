350d49e818a3280abb7a8b2382786d076d47f66f
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-19 17:31:03 +0100

    installed drf

--- nova_print/nova_print/urls.py
+++ nova_print/nova_print/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('emacs_api.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]

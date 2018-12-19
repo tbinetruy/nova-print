a1193ff4af04511bcd6994cf31950d44dbf09076
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-19 23:39:58 +0100

    updated user serializer

--- nova_print/emacs_api/serializers.py
+++ nova_print/emacs_api/serializers.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from emacs_api.models import Document
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
+
+class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Document
+        fields = ('pk', 'author', 'toc', 'title', 'subtitle', 'org', 'theme_color')

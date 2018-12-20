573fec9fb7d67cd2f98dff67fb4b6fe89d1a4392
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-20 15:57:37 +0100

    refactored login endpoint into API class

---
+++ nova_print/static/js/frontend/src/API.js
@@ -0,0 +1,26 @@
+const clientId = "F9pKxCwq8n9mr41YwLjFjAoaXOL7FOs7rEvzirTA";
+const clientSecret =
+  "8sfNsRo5wfSYmD0hzqPesPWCcTnoU7SyYsbgpdvypj2riZSTFzPs01mnlOb6m9MVbVhIgp7jfmMNAAqC5EeyWk08QiDJDxyQqttwEFkav13ZygCU71HsCbDb7GhYaoJ4";
+
+class API {
+  constructor() {}
+  async login(username, password) {
+    const url = `/authenticate/?client_id=${clientId}&client_secret=${clientSecret}&grant_type=password`;
+
+    const body = JSON.stringify({
+      username,
+      password,
+    });
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    const options = {method: "POST", body, headers};
+
+    const resp = await fetch(url, options);
+    const user = await resp.json();
+
+    return user;
+  }
+}
+
+export default API;
--- nova_print/static/js/frontend/src/App.js
+++ nova_print/static/js/frontend/src/App.js
@@ -2,10 +2,7 @@ import React, {Component} from "react";
 import "./App.css";
 import {colorList} from "./constants.js";
 import novaPrintScreenshot from "./img/nova-print-screenshot.png";
-
-const clientId = "F9pKxCwq8n9mr41YwLjFjAoaXOL7FOs7rEvzirTA";
-const clientSecret =
-  "8sfNsRo5wfSYmD0hzqPesPWCcTnoU7SyYsbgpdvypj2riZSTFzPs01mnlOb6m9MVbVhIgp7jfmMNAAqC5EeyWk08QiDJDxyQqttwEFkav13ZygCU71HsCbDb7GhYaoJ4";
+import API from "./API.js";
 
 const Intro = props => {
   const styles = {
@@ -379,19 +376,9 @@ class App extends Component {
     e.preventDefault();
 
     const {username, password} = this.state;
-    const url = `/authenticate/?client_id=${clientId}&client_secret=${clientSecret}&grant_type=password`;
-    const body = JSON.stringify({
-      username,
-      password,
-    });
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    const options = {method: "POST", body, headers};
 
     try {
-      const resp = await fetch(url, options);
-      const user = await resp.json();
+      const user = await new API().login(username, password);
 
       if (user.token) {
         this.setState({user, errorMsg: ""});

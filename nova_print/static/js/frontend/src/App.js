1cfaf0352adc851b43dccce3678c32915e1e6cc6
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-20 15:51:52 +0100

    refactored header

--- nova_print/static/js/frontend/src/App.js
+++ nova_print/static/js/frontend/src/App.js
@@ -252,6 +252,31 @@ const DocumentViewer = props => {
   );
 };
 
+const Header = props => {
+  const styles = {
+    header: {
+      display: "flex",
+      flex: 1,
+      height: "3rem",
+      padding: "1rem",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+    },
+    username: {
+      margin: "0 1rem",
+    },
+  };
+  return (
+    <div style={styles.header}>
+      <div style={styles.logout}>
+        <span style={styles.username}>Hello {props.username}</span>
+        <button onClick={props.logout} key={2}>
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -441,16 +466,6 @@ class App extends Component {
         display: "flex",
         flexDirection: "column",
       },
-      header: {
-        display: "flex",
-        flex: 1,
-        height: "3rem",
-        padding: "1rem",
-        backgroundColor: "rgba(0, 0, 0, 0.1)",
-      },
-      username: {
-        margin: "0 1rem",
-      },
     };
     return (
       <div className="App">
@@ -467,14 +482,7 @@ class App extends Component {
           </div>
         ) : (
           <div style={styles.pageWrapper}>
-            <div style={styles.header}>
-              <div style={styles.logout}>
-                <span style={styles.username}>Hello {this.state.username}</span>
-                <button onClick={this.logout} key={2}>
-                  Logout
-                </button>
-              </div>
-            </div>
+            <Header username={this.state.username} logout={this.logout} />
             <div style={styles.dashboardWrapper}>
               <DocumentList
                 documents={this.state.documents}

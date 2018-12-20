d5051ac06b9709dd221aedd7f94746f21a70761d
Thomas Binetruy <thomas.binetruy@telecom-paristech.fr>
2018-12-20 19:57:57 +0100

    remade model with foreignkey to figures

--- nova_print/emacs_api/admin.py
+++ nova_print/emacs_api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from emacs_api.models import Document
+from emacs_api.models import Document, Figure
 
 # Register your models here.
 
 class DocumentAdmin(admin.ModelAdmin):
     pass
 
+class FigureAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Document, DocumentAdmin)
+admin.site.register(Figure, FigureAdmin)
--- nova_print/emacs_api/models.py
+++ nova_print/emacs_api/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.author.id, filename)
+
+class Figure(models.Model):
+    title = models.CharField(max_length=50,  blank=True)
+    image = models.FileField(upload_to=user_directory_path)
+
 class Document(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    images = models.ForeignKey(Figure, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=50)
     theme_color = models.CharField(max_length=6)
     toc = models.BooleanField()
     org = models.TextField()
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
-class Image(models.Model):
-    title = models.CharField(max_length=50,  blank=True)
-    image = models.FileField(upload_to=user_directory_path)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
